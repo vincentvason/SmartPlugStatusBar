880e2c499cac2c217e86e71e6f46251d80ad5809
Vincent Vason <the.vincent@hotmail.com>
2016-01-14 03:49:30 +0700

    Add Time Get

--- SmartPlug/SmartPlug.ino
+++ SmartPlug/SmartPlug.ino
@@ -13,25 +13,119 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-
+#include <WiFiUdp.h>
+#include <TimeLib.h> 
 
 
 const char* ssid = "CE-ESL";
 const char* password = "ceeslonly";
 
 const char* host = "api.anto.io";
+const char* host_time = "api.timezonedb.com";
 const int httpsPort = 80;
 
+int timeh;
+int timem;
 int modes;
 int lights;
+int settimeh;
+int settimem;
 
 //---------------------------------------------------------------------------------------------
 void blinkgreen () { digitalWrite(12, HIGH);digitalWrite(13, LOW);digitalWrite(14, LOW); }
 void blinkblue() { digitalWrite(12, LOW);digitalWrite(13, HIGH);digitalWrite(14, LOW); }
+void blinkwarnblue(int x) {
+  digitalWrite(12, LOW);digitalWrite(13, HIGH);digitalWrite(14, LOW); 
+  delay(x);
+  digitalWrite(12, HIGH);digitalWrite(13, LOW);digitalWrite(14, HIGH);
+  }
+void blinkwarnpink(int x) {
+  digitalWrite(12, LOW);digitalWrite(13, HIGH);digitalWrite(14, LOW); 
+  delay(x);
+  digitalWrite(12, HIGH);digitalWrite(13, LOW);digitalWrite(14, HIGH);
+  }
 void blinkpink () { digitalWrite(12, LOW);digitalWrite(13, HIGH);digitalWrite(14, HIGH); }
 void blinkred () { digitalWrite(12, LOW);digitalWrite(13, LOW);digitalWrite(14, HIGH); }
 
 //---------------------------------------------------------------------------------------------
+void gettime()
+{
+   WiFiClient client;
+  //Serial.print("connecting to ");
+  Serial.print("GET TIME ");
+  Serial.print(" STATUS :  ");
+  if (!client.connect(host, httpsPort)) {
+    Serial.print("F");
+    return;
+  }
+
+  String url = "/?zone=Asia/Bangkok&format=json&key=BU3CDABKAG68";
+  //Serial.print("requesting URL: ");
+  Serial.print("P");
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host_time + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  //Serial.println("headersrequest sent");
+  String line = "";
+  while (!client.available());
+  while (client.available()) {
+  line+=client.readStringUntil('\n');
+
+  uint32_t index = line.indexOf('{');
+  line.remove(0, index);
+
+//  Serial.println("reply was:");
+//  Serial.println("==========");
+//  Serial.println(line);
+//  Serial.println("==========");
+//  Serial.println("closing connection");
+    if (line == "\r") {
+      Serial.print("P");
+      break;
+    }
+  }
+  if (line.startsWith("{\"status\":\"OK\"")) {
+    //Serial.println("esp8266/Arduino CI successfull!");
+    Serial.print("P");
+  } else {
+    Serial.print("F");
+  }
+
+    unsigned long long int value_time = 0;
+    char num1_time = 0;
+    int count_time = 0;
+    int cha_time = 0;
+   while (count_time < 30)
+    {
+        if(line[cha_time] == '\"') count_time++;
+        cha_time++;
+    }
+    cha_time++;
+
+    while(value_time < 1000000000)
+    {
+
+      value_time = value_time*10;
+      num1_time = line[cha_time];
+      value_time = value_time + (num1_time - '0');
+      cha_time++;
+      if(line[cha_time] == '\"') count_time++;
+    }
+    //printf("%d",value_time);
+
+  time_t time = value_time;
+  timeh = hour(time);
+  timem = minute(time);
+  Serial.print("TIME GET : ");
+  Serial.print(timeh);
+  Serial.print(" ");
+  Serial.println(timem);
+     
+   }
+//-------------------------------------------------------------
 void getdata(String data) {
   WiFiClient client;
   //Serial.print("connecting to ");
@@ -39,7 +133,7 @@ void getdata(String data) {
   Serial.print(data);
   Serial.print(" STATUS :  ");
   if (!client.connect(host, httpsPort)) {
-    Serial.print(" -- FAILED --");
+    Serial.print("F");
     return;
   }
 
@@ -66,7 +160,7 @@ void getdata(String data) {
     //Serial.println("esp8266/Arduino CI successfull!");
     Serial.print("P");
   } else {
-    Serial.print(" -- FAILED --");
+    Serial.print("F");
   }
 //  Serial.println("reply was:");
 //  Serial.println("==========");
@@ -107,6 +201,8 @@ void getdata(String data) {
   }
   if(data = "ModeSet") modes = value;
   if(data = "LightSet") lights = value;
+  if(data = "TimerH") settimeh = value;
+  if(data = "TimerM") settimem = value;
   Serial.print(" Mode Get : "); 
   Serial.println(modes);
 }
@@ -120,7 +216,7 @@ void setdata(String data, int valueset) {
   Serial.print(valueset);
   Serial.print("STATUS :  ");
   if (!client.connect(host, httpsPort)) {
-    Serial.print(" -- FAILED --");
+    Serial.print("F");
     return;
   }
 
@@ -145,7 +241,7 @@ void setdata(String data, int valueset) {
     //Serial.println("esp8266/Arduino CI successfull!");
     Serial.print("P");
   } else {
-    Serial.print(" -- FAILED --");
+    Serial.print("F");
   }
 //  Serial.println("reply was:");
 //  Serial.println("==========");
@@ -215,6 +311,7 @@ void setup() {
 }
 void loop() {
   getdata("ModeSet");
+  gettime();
   if(modes == 1) {
     getdata("NormalLight");
     if(lights == 1) blinkgreen();
@@ -225,5 +322,6 @@ void loop() {
     }
   if(modes == 3) {
     blinkblue();
+    
     }
 }
